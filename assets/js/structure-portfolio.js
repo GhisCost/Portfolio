e05d51dd1d5c807fad90ff0b0b2f9a88d4c943ad
unknown <ghis.cost@gmail.com>
2025-08-01 16:58:59 +0200

    début de la nav-mobile

--- assets/js/structure-portfolio.js
+++ assets/js/structure-portfolio.js
@@ -32,3 +32,22 @@ function loadPage(url, direction) {
     })
     .catch(err => console.error('Erreur chargement page:', err));
 }
+
+// JS nav-mobile
+
+function faireApparaitre(){
+  document.getElementById("menu-nav-mobile").classList.toggle("montrer")
+};
+
+window.onclick = function(event) {
+  if (!event.target.matches('#ouverture')) {
+    var dropdowns = document.getElementsByClassName("ul-nav-mobile");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('montrer')) {
+        openDropdown.classList.remove('montrer');
+      }
+    }
+  }
+} 
